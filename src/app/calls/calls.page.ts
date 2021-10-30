e72e086e3600af7bf2e4a7c7c27913c5c8d6a12a
Megha Shrivastava <megha0071994@mail.com>
2021-10-30 18:00:56 +0530

    first commit

---
+++ src/app/calls/calls.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-calls',
+  templateUrl: './calls.page.html',
+  styleUrls: ['./calls.page.scss'],
+})
+export class CallsPage implements OnInit {
+  @ViewChild('mySlider') slider: IonSlides;
+  activeSlide = 0;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  slideChanged(event) {
+    this.slider.getActiveIndex().then((index: number) => {
+      this.activeSlide = index;
+    });
+  }
+}
